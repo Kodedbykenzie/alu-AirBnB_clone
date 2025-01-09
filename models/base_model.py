89d3d7c105ee4e59ca791b4e92ba1ec7f1062b53
Glorycodess <g.paul@alustudent.com>
2025-01-09 14:31:33 +0100

    test and models

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/python3
 """Defines the BaseModel class."""
 import models
@@ -47,3 +48,36 @@ class BaseModel:
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+=======
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())  # Generate a unique id
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update the updated_at attribute to the current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance"""
+        dict_repr = self.__dict__.copy()  # Copy the object's dictionary
+        dict_repr["__class__"] = self.__class__.__name__  # Add class name for easy identification
+        dict_repr["created_at"] = self.created_at.isoformat()  # Convert to ISO string format
+        dict_repr["updated_at"] = self.updated_at.isoformat()  # Convert to ISO string format
+        return dict_repr
+
+>>>>>>> test and models
